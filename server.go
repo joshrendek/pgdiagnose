cafe8f8f48340f6c3e02c645c66536aac691282d
Will Leinweber <will@bitfission.com>
2014-05-13 13:51:36 -0700

    move to server

--- server.go
+++ server.go
@@ -12,26 +12,41 @@ import (
 )
 
 type JobParams struct {
-	Url string `json:"url" binding:"required"`
+	URL string `json:"url" binding:"required"`
+}
+
+func createJob(db *sql.DB) (id string, err error) {
+	row := db.QueryRow("INSERT INTO results DEFAULT VALUES returning id")
+	err = row.Scan(&id)
+	if err != nil {
+		log.Print("%v", err)
+		return "", err
+	}
+	fmt.Println("new job id: %v", id)
+	return id, nil
+
 }
 
 func create(params JobParams, db *sql.DB) (int, string) {
-	var id string
-	row := db.QueryRow("INSERT INTO results DEFAULT VALUES returning id")
-	err := row.Scan(&id)
+	id, err := createJob(db)
 	if err != nil {
 		log.Print("%v", err)
 		return 500, "error"
 	}
-	fmt.Println("id: %v", id)
 	return 200, "foo: " + id
 }
 
 func setupDB() *sql.DB {
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	connstring := os.Getenv("DATABASE_URL")
+	if connstring == "" {
+		connstring = "dbname=pgdiagnose sslmode=disable"
+	}
+
+	db, err := sql.Open("postgres", connstring)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	_, err = db.Exec("select 1")
 	if err != nil {
 		log.Fatal(err)
