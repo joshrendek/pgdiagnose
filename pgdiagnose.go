cafe2dc12845a4b373c6d402599c0557656f89b1
Will Leinweber <will@bitfission.com>
2014-05-15 14:49:04 -0700

    better hit rate

--- pgdiagnose.go
+++ pgdiagnose.go
@@ -263,16 +263,19 @@ type hitRateResult struct {
 
 func hitRateCheck(db *sqlx.DB) (results []hitRateResult) {
 	query := `
-SELECT
-  'index hit rate' AS name,
-  coalesce((sum(idx_blks_hit)) / nullif(sum(idx_blks_hit + idx_blks_read), 0),0) AS ratio
-FROM pg_statio_user_indexes
-UNION ALL
-SELECT
-  'table hit rate' AS name,
-  coalesce(sum(heap_blks_hit) / nullif(sum(heap_blks_hit) + sum(heap_blks_read), 0),0) AS ratio
-FROM pg_statio_user_tables
-; `
+WITH rates AS (
+	SELECT
+		'index hit rate' AS name,
+		sum(idx_blks_hit) / nullif(sum(idx_blks_hit + idx_blks_read), 0) AS ratio
+	FROM pg_statio_user_indexes
+	UNION ALL
+	SELECT
+		'table hit rate' AS name,
+		sum(heap_blks_hit) / nullif(sum(heap_blks_hit) + sum(heap_blks_read), 0) AS ratio
+	FROM pg_statio_user_tables
+)
+SELECT * FROM rates WHERE ratio < 0.99
+;`
 	err := db.Select(&results, query)
 	errDie(err)
 	return results
