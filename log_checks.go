cafec7a1999362c13723b81d808fe8d9cba73319
Will Leinweber <will@bitfission.com>
2014-05-23 08:41:20 -0700

    test load

--- log_checks.go
+++ log_checks.go
@@ -9,6 +9,44 @@ import (
 	"strings"
 )
 
+func CheckLogs(database string, logplexURL string) []Check {
+	return checkLogsFromBytes(database, getData(logplexURL))
+}
+
+func checkLogsFromBytes(database string, data []byte) []Check {
+	log := findLogForDatabase(database, data)
+
+	v := make([]Check, 6)
+	v[0] = checkLoadOnLog(log)
+
+	return v
+}
+
+type loadAvgs struct {
+	LoadAvg1M  float64
+	LoadAvg5M  float64
+	LoadAvg15M float64
+}
+
+func checkLoadOnLog(log DatabaseLog) Check {
+	if (log == DatabaseLog{}) {
+		return Check{"Load", "skipped", nil}
+	} else {
+		load := loadAvgs{log.LoadAvg1M, log.LoadAvg5M, log.LoadAvg15M}
+		return checkLoad(load)
+	}
+}
+
+func checkLoad(load loadAvgs) Check {
+	if load.LoadAvg1M > 3 {
+		return Check{"Load", "red", load}
+	} else if load.LoadAvg1M > 1 {
+		return Check{"Load", "yellow", load}
+	} else {
+		return Check{"Load", "green", nil}
+	}
+}
+
 func getData(logplexURL string) []byte {
 	resp, err := http.Get(logplexURL)
 	if err != nil {
@@ -24,7 +62,7 @@ func getData(logplexURL string) []byte {
 	return body
 }
 
-func findLogsForDatabase(logs []byte, database string) []byte {
+func findLogLineForDatabase(database string, logs []byte) []byte {
 	lines := bytes.Split(logs, []byte("\n"))
 	search := []byte("source=" + database)
 	for i := len(lines) - 1; i >= 0; i-- {
@@ -35,7 +73,12 @@ func findLogsForDatabase(logs []byte, database string) []byte {
 	return nil
 }
 
+func findLogForDatabase(database string, logs []byte) DatabaseLog {
+	return parseLog(findLogLineForDatabase(database, logs))
+}
+
 type DatabaseLog struct {
+	Source         string  `logfmt:"source="`
 	LoadAvg1M      float64 `logfmt:"sample#load-avg-1m"`
 	LoadAvg5M      float64 `logfmt:"sample#load-avg-5m"`
 	LoadAvg15M     float64 `logfmt:"sample#load-avg-15m"`
@@ -53,8 +96,8 @@ type DatabaseLog struct {
 func parseLog(log []byte) (parsed DatabaseLog) {
 	logfmt.Unmarshal(log, &parsed)
 	parsed.MemoryTotal = removeKb(parsed.RawMemoryTotal)
-	parsed.MemoryCached = removeKb(parsed.RawMemoryTotal)
-	parsed.MemoryFree = removeKb(parsed.RawMemoryTotal)
+	parsed.MemoryCached = removeKb(parsed.RawMemoryCached)
+	parsed.MemoryFree = removeKb(parsed.RawMemoryFree)
 	parsed.MemoryPostgres = removeKb(parsed.RawMemoryPostgres)
 	return parsed
 }
